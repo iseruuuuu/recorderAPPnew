20be9bf9f48f7bc603b754ebcb3b91d46c24d2c6
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2020-10-10 21:02:39 +0900

    Initial Commit

---
+++ recorderAPPnew/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  recorderAPPnew
+//
+//  Created by user on 2020/10/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
